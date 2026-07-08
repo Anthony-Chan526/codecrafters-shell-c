710bd8a809d5e042aeb748b364f99d6f3771a041
Anthony Chan <khc1g24@soton.ac.uk>
2026-07-08 10:28:43 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
   char command[1024];
   fgets(command, sizeof(command), stdin);
   command[strcspn(command, "\n")] = '\0';
-  printf("%s: command not found\n", command)
+  printf("%s: command not found\n", command);
 
   return 0;
 }
