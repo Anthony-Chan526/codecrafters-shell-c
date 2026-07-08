cb1967ab18d9ee97e5f4e05bee23548c93c89e93
Anthony Chan <khc1g24@soton.ac.uk>
2026-07-08 11:24:53 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[]) {
     fgets(input, sizeof(input), stdin);
     input[strlen(input) - 1] = '\0';
     if (strcmp(input, "exit ") == 0) { break; }
-    else if (strcmp(input, "echo ") == 0) { printf("%s\n", input + 5); }
-    else if (strcmp(input, "type ") == 0) {
+    else if (strncmp(input, "echo ". 5) == 0) { printf("%s\n", input + 5); }
+    else if (strncmp(input, "type ", 5) == 0) {
       char *arg = input + 5;
       if (strcmp(arg, "echo") && strcmp(arg, "exit") && strcmp(arg, "type")) {
         printf("%s: not found\n", arg);
