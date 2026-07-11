fa11bbd645148ec9fc032f75068e84ecf89cf026
Anthony Chan <khc1g24@soton.ac.uk>
2026-07-11 14:32:48 +0800

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[]) {
       }
     } else if (strcmp(input, "pwd") == 0) {
       char cwd[1024]; 
-      printf("%s", getcwd(cwd, sizeof(cwd)));
+      printf("%s\n", getcwd(cwd, sizeof(cwd)));
     } else { 
             char input_copy[1024];
             strcpy(input_copy, input);
